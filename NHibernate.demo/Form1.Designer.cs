d6510670e28d7923e15c1f4e35e87b92c6e1ae6f
Leon520 <1506816370@qq.com>
2016-10-05 00:47:54 +0800

    1、分开测试NHibernate

--- NHibernate.demo/Form1.Designer.cs
+++ NHibernate.demo/Form1.Designer.cs
@@ -35,33 +35,47 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage4.SuspendLayout();
             this.SuspendLayout();
             // 
             // listView1
             // 
             this.listView1.FullRowSelect = true;
             this.listView1.GridLines = true;
-            this.listView1.Location = new System.Drawing.Point(12, 50);
+            this.listView1.Location = new System.Drawing.Point(12, 116);
             this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(719, 260);
+            this.listView1.Size = new System.Drawing.Size(719, 216);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 7);
+            this.button1.Location = new System.Drawing.Point(6, 6);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "HSQL查询";
+            this.button1.Text = "from";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(93, 7);
+            this.button2.Location = new System.Drawing.Point(6, 7);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
@@ -71,7 +85,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(174, 7);
+            this.button3.Location = new System.Drawing.Point(6, 6);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
@@ -81,7 +95,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(255, 7);
+            this.button4.Location = new System.Drawing.Point(189, 6);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 4;
@@ -91,7 +105,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(336, 7);
+            this.button5.Location = new System.Drawing.Point(87, 35);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(95, 23);
             this.button5.TabIndex = 5;
@@ -101,7 +115,7 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(437, 7);
+            this.button6.Location = new System.Drawing.Point(87, 7);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(92, 23);
             this.button6.TabIndex = 6;
@@ -109,20 +123,132 @@
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(87, 6);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(96, 23);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Linq分页";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(12, 7);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(719, 103);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button11);
+            this.tabPage1.Controls.Add(this.button10);
+            this.tabPage1.Controls.Add(this.button9);
+            this.tabPage1.Controls.Add(this.button8);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(711, 77);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "HQL";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(249, 6);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(88, 23);
+            this.button11.TabIndex = 9;
+            this.button11.Text = "select 字段";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(168, 6);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 8;
+            this.button10.Text = "select";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(87, 6);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 7;
+            this.button9.Text = "from as";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(8, 35);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(73, 23);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "where";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.button7);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(711, 77);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Linq";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(711, 77);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Criteria";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.button2);
+            this.tabPage4.Controls.Add(this.button6);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(711, 77);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "SQL";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(743, 322);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(743, 347);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -136,6 +262,16 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
     }
 }
 
--- NHibernate.demo/Form1.cs
+++ NHibernate.demo/Form1.cs
@@ -45,22 +45,88 @@ namespace NHibernate.demo
         private void button4_Click(object sender, EventArgs e)
         {
             ISession session = NHibernateHelper.CreateSession();
-            var result = (from orders in session.Query<Orders>()
-                          join customer in session.Query<Customers>() on orders.CustomerId equals customer.CustomerId
-                          select orders).ToList<Orders>();
-            Tool.FillListView<Orders>(result, listView1);
+            var result = from orders in session.Query<Orders>()
+                         join customer in session.Query<Customers>() on orders.CustomerId equals customer.CustomerId
+                         select orders;
+            var list = result.ToList<Orders>();
+            Tool.FillListView<Orders>(list, listView1);
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
             ISession session = NHibernateHelper.CreateSession();
-            var list = session.CreateQuery("from Products").SetFirstResult((1 - 1) * 10).SetMaxResults(10).List<Products>();
+            IQuery query = session.CreateQuery("from Products").SetFirstResult((1 - 1) * 10).SetMaxResults(10);
+            var list = query.List<Products>();
             Tool.FillListView<Products>(list, listView1);
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ISession session = NHibernateHelper.CreateSession();
+            var query = from o in session.Query<Orders>()
+                        where o.CustomerId == "ALFKI"
+                        select o;
+            query = query.Skip((1 - 1) * 10).Take(10);
+            var list = query.ToList();
+            Tool.FillListView(list, listView1);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ISession session = NHibernateHelper.CreateSession();
+            IQuery query = session.CreateQuery("from Customers c where c.CustomerID='ANATR'");
+            IList<Customers> list = query.List<Customers>();
+            Tool.FillListView(list, listView1);
+            // 数学操作符：+, -, *, /
+            // 真假比较操作符：=, >=, <=, <>, !=, like
+            // 逻辑操作符：and, or, not
+            // 字符串连接操作符：||  
+            // SQL标量函数：upper(),lower() 
+            // 没有前缀的( )：表示分组
+            // in, between, is null
+            // 位置参数：?
+            // 命名参数：:name, :start_date, :x1
+            // SQL文字：'foo', 69, '1970-01-01 10:00:01.0'
+            // 枚举值或常量：Color.Tabby
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            ISession session = NHibernateHelper.CreateSession();
+            IQuery query = session.CreateQuery("from Customers as c");
+            //IQuery query = session.CreateQuery("from Customers c");
+            var list = query.List<Customers>();
+            Tool.FillListView(list, listView1);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            ISession session = NHibernateHelper.CreateSession();
+            IQuery query = session.CreateQuery("select c from Customers c");
+            IList<Customers> list = query.List<Customers>();
+            Tool.FillListView(list, listView1);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            ISession session = NHibernateHelper.CreateSession();
+            IQuery query = session.CreateQuery("select c.CustomerId,c.CompanyName from Customers c");
+            IList<object[]> list = query.List<object[]>();
+            IList<Customers> list1 = new List<Customers>();
+            foreach (var item in list)
+            {
+                list1.Add(new Customers
+                {
+                    CustomerId = (string)item[0],
+                    CompanyName = (string)item[1]
+                });
+            }
+            Tool.FillListView(list1, listView1);
         }
     }
 }
